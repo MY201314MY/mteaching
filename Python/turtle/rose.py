157b694332930b898e347bd2b43bde6841b01ec9
moonpi <3477669308@qq.com>
2021-02-24 23:06:25 +0800

    draw a rose with turtle.

---
+++ Python/turtle/rose.py
@@ -0,0 +1,61 @@
+import turtle
+import time
+
+if __name__ == '__main__':
+    turtle.penup()
+    turtle.left(90)
+    turtle.fd(200)
+    turtle.pendown()
+    turtle.fillcolor("red")
+
+    turtle.begin_fill()
+
+    turtle.circle(10, 180)
+
+    turtle.circle(25, 110)
+
+    turtle.left(50)
+
+    turtle.circle(60, 45)
+
+    turtle.circle(20, 170)
+
+    turtle.right(24)
+
+    turtle.fd(30)
+
+    turtle.left(10)
+
+    turtle.circle(30, 110)
+
+    turtle.fd(20)
+
+    turtle.left(40)
+
+    turtle.circle(90, 70)
+
+    turtle.circle(30, 150)
+
+    turtle.right(30)
+
+    turtle.fd(15)
+
+    turtle.circle(80, 90)
+
+    turtle.left(15)
+
+    turtle.fd(45)
+
+    turtle.right(165)
+
+    turtle.fd(20)
+
+    turtle.left(155)
+
+    turtle.circle(150, 80)
+
+    turtle.left(50)
+
+    turtle.circle(150, 90)
+
+    turtle.end_fill()
