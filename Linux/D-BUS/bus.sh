6d1243b7fceaf021cf1e3ff10f679c910175fcf2
MY201314MY <3477669308@qq.com>
2021-05-14 09:59:50 +0800

    Create bus.sh

---
+++ Linux/D-BUS/bus.sh
@@ -0,0 +1,2 @@
+dbus-send --system --print-reply --dest=org.freedesktop.DBus / org.freedesktop.DBus.ListActivatableNames
+dbus-send --session --print-reply --dest=org.freedesktop.DBus / org.freedesktop.DBus.ListActivatableNames
