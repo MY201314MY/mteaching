a43deb08d187e7f805b2aa4cb9744420345c865e
MY201314MY <3477669308@qq.com>
2021-06-18 15:53:17 +0800

    Update state.py

--- Linux/ST/STM32MP157CAC3-DK2/BLE/connect/ESP32/state.py
+++ Linux/ST/STM32MP157CAC3-DK2/BLE/connect/ESP32/state.py
@@ -1,17 +1,19 @@
 import dbus
 
+#reference URL:https://blog.csdn.net/zhuo_lee_new/article/details/106626680
 bus = dbus.SystemBus()
 
-interface = dbus.Interface(object=bus.get_object(bus_name="org.bluez", object_path="/org/bluez/hci0/dev_30_AE_A4_12_CB_56"),
-                            dbus_interface="org.freedesktop.DBus.Properties")
+interface = dbus.Interface(object=bus.get_object(bus_name="org.bluez", object_path="/org/bluez/hci0/dev_30_AE_A4_12_CB_56/service0028/char0029"),
+                            dbus_interface="org.bluez.GattCharacteristic1")
 
-result = interface.GetAll("org.bluez.Device1")
-print(result)
+result = interface.ReadValue([])
+print("Read Value:{0}",format(result))
 
-interface = dbus.Interface(object=bus.get_object(bus_name="org.bluez", object_path="/org/bluez/hci0/dev_30_AE_A4_12_CB_56"),
-                            dbus_interface="org.bluez.Device1")
+choice = input("Please input on/off to control the LED:")
 
-print(interface)
-result = interface.Connect()
-print(result)
+if choice == "on":
+    print("Turn ON the LED.")
+else:
+    print("Turn ON the LED.")
 
+interface.WriteValue(choice.encode(), [])
