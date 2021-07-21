b50773e695a7020b07a341fd01e9012ee78647c5
MY201314MY <3477669308@qq.com>
2021-07-21 15:37:24 +0800

    dbus example

---
+++ Linux/D-BUS/dbus/cmake-build-debug/CMakeFiles/dbus.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/CLion/dbus/gdbus.c" "/home/pi/CLion/dbus/cmake-build-debug/CMakeFiles/dbus.dir/gdbus.c.o"
+  "/home/pi/CLion/dbus/main.c" "/home/pi/CLion/dbus/cmake-build-debug/CMakeFiles/dbus.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/dbus-1.0"
+  "/usr/lib/x86_64-linux-gnu/dbus-1.0/include"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "/usr/include/gio-unix-2.0"
+  "../-I/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Linux/D-BUS/dbus/cmake-build-debug/CMakeFiles/dbus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dbus.dir/gdbus.c.o"
+  "CMakeFiles/dbus.dir/main.c.o"
+  "dbus"
+  "dbus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dbus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
